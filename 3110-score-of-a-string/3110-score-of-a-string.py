8b3195dc9702d5334f4b370c8b2d6f58cabae0d5
S-vani <146605182+S-vani@users.noreply.github.com>
2025-08-31 12:55:13 -0400

    Time: 1 ms (37.26%), Space: 17.7 MB (45.26%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.py
+++ 3110-score-of-a-string/3110-score-of-a-string.py
@@ -1,9 +1,8 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
         total = 0
-        string_list = list(s)
         for letter in range(len(s)-1):
-            difference = ord(string_list[letter]) - ord(string_list[letter + 1])
+            difference = ord(s[letter]) - ord(s[letter + 1])
             total += abs(difference)
         
         return total
