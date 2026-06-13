b04219891c03e4e1a72c27fa40ad69e3aa701163
Shayan Vanishoja <shayan.vanishoja86@gmail.com>
2026-06-13 16:17:54 -0400

    Time: 10 ms (52.49%), Space: 19.4 MB (34.27%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -4,8 +4,7 @@ class Solution:
         for word in words:
             total = 0
             for letter in word:
-                int_value = ord(letter) - 97
-                total += weights[int_value]
+                total += weights[ord(letter) - 97]
             let = chr(122 - (total % 26))
             final = final + let
         return final
