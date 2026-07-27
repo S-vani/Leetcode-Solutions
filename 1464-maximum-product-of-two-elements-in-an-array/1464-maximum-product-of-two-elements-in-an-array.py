bde9b20175a1a72be66fd883526e81ccbb305907
Shayan Vanishoja <shayan.vanishoja86@gmail.com>
2026-07-26 22:15:55 -0400

    Time: 7 ms (16.3%), Space: 19.4 MB (10.65%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -4,11 +4,16 @@ class Solution:
             return (n1-1)*(n2-1)
         if len(nums) == 2:
             return _calculate_product(nums[0], nums[1])
-        largest = 0
-        for i in range(len(nums)):
-            for n in range(i+1, len(nums)):
-                if i != len(nums) - 1:
-                    largest = max(largest, _calculate_product(nums[i], nums[n]))
-        
-        return largest
+        largest_num_index = 0
+        second_largest_num_index = 1
+        for i in range(1, len(nums)):
+            if nums[i] > nums[largest_num_index]:
+                second_largest_num_index = largest_num_index
+                largest_num_index = i
+            elif nums[i] > nums[second_largest_num_index]:
+                second_largest_num_index = i
+            print(f"{largest_num_index} and {second_largest_num_index}")
+
+        return _calculate_product(nums[largest_num_index], nums[second_largest_num_index])
+            
         
