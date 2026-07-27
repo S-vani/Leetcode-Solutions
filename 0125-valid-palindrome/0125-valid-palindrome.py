f8c63d25c50c2956a2353ee6fae1c32361d1f92f
Shayan Vanishoja <shayan.vanishoja86@gmail.com>
2026-07-27 17:10:07 -0400

    Time: 10 ms (36.42%), Space: 19.7 MB (52.21%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        converted_str = ""
+        for letter in s:
+            if letter.isalnum():
+                converted_str += letter.lower()
+        i = 0
+        j = len(converted_str) - 1
+        print(converted_str)
+        while i < j:
+            if converted_str[i] != converted_str[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
